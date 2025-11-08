6c840d8680b63605a8981ce4a7e0ed3da404cbd9
Abdulhakim <abdlhakimnesrugmail.com>
2025-11-08 11:50:57 +0300

    add view_expenses as function which check the expenses is add or not

--- expenses_in_python.py
+++ expenses_in_python.py
@@ -1,6 +1,19 @@
 expenses = []  # list to store expenses
 
 def add_expense(description, amount):
-    """Add a new expense to the list"""
     expenses.append({"description": description, "amount": float(amount)})      
     print("Expense added successfully!")
+
+
+def view_expenses():
+    if not expenses:
+        print("No expenses yet. Add one first!")
+        return
+    total = 0
+    print("\nDescription      Amount")
+    print("-"*25)
+    for item in expenses:
+        print(f"{item['description']:15}  ${item['amount']}")
+        total += item['amount']
+    print("-"*25)
+    print(f"Total Spent: ${total}")
